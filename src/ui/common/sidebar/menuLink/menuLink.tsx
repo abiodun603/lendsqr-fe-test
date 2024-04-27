0b9484a9b0f715d74ee74c79cf5c9234f62183d0
abiodun <abioduncsc@gmail.com>
2024-04-27 11:25:43 +0100

    fix: make sidebar active on deep linking

--- src/ui/common/sidebar/menuLink/menuLink.tsx
+++ src/ui/common/sidebar/menuLink/menuLink.tsx
@@ -6,26 +6,32 @@ import { usePathname } from 'next/navigation';
 
 const MenuLink = ({ item }: any) => {
   const pathname = usePathname();
+  const segments = pathname.split('/'); // Split the pathname into segments
 
+  
+  const sanitizedPathname = segments.length === 2 ? `/${segments[1]}` :  `/${segments[1]}/${segments[2]}`;
+
+  console.log(sanitizedPathname)
   return (
     <Link
       href={item.path}
       className={`h-[40px] w-full flex items-center  pl-5   my-[5px] space-x-3 hover:bg-[#F3FCFC] ${
-        pathname === item.path && 'bg-[#F3FCFC] border border-l-[3px] border-transparent border-l-primary'
+        sanitizedPathname === item.path && 'bg-[#F3FCFC] border border-l-[3px] border-transparent border-l-primary'
       }`}
     >
        {/* <span className={`text-lg ${pathname === item.path ? 'text-p800 ' : 'text-n900'}`}> */}
         <Image
-          src={pathname === item.path ? item.imageActive : item.image }
+          src={sanitizedPathname === item.path ? item.imageActive : item.image }
           alt='icon'
           height={38}
           width={38}
           className={`h-auto w-auto`}          
         />
       {/* </span> */}
-      <p className={`${pathname === item.path ? 'text-[16px] font-normal text-b200' : 'text-[16px] font-normal text-b200'}`}>{item.title}</p>
+      <p className={`${sanitizedPathname === item.path ? 'text-[16px] font-normal text-b200' : 'text-[16px] font-normal text-b200'}`}>{item.title}</p>
     </Link>
   );
 };
 
 export default MenuLink;
+ 
